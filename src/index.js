1be954b632e23b43bf832483e21dd5dc01317f7f
wdra233 <wangyawang666@gmail.com>
2021-06-12 15:43:30 -0700

    adding router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Register from './containers/register/register';
+import Login from './containers/login/login';
+import Main from './containers/main/main';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render((
+    <HashRouter>
+        <Switch>
+            <Route path='/register' component={Register}></Route>
+            <Route path='/login' component={Login}></Route>
+            <Route component={Main}></Route>
+        </Switch>
+    </HashRouter>
+), 
+document.getElementById('root'));
