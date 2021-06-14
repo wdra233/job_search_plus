2fab911e396b782ab36e20d8ced889a8b648a067
wdra233 <wangyawang666@gmail.com>
2021-06-14 16:46:03 -0700

    adding login and register page

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,8 +1,51 @@
 import React, {Component} from 'react';
-export default class Login extends Component {
+
+import {
+    NavBar,
+    WingBlank,
+    WhiteSpace,
+    InputItem,
+    Button
+} from 'antd-mobile';
+
+import Logo from '../../components/logo/logo'; 
+
+export default class Register extends Component {
+    state = {
+        username: '',
+        password: '',
+    };
+
+    login = () => {
+        console.log(this.state);
+    }
+
+    handleChange = (attributeName, val) => {
+        this.setState({
+            [attributeName]: val
+        });
+    }
+
+    toRegister = () => {
+        this.props.history.replace('/register');
+    }
+    
     render() {
         return (
-            <div>Login</div>
+            <div>
+                <NavBar>Job Radar</NavBar>
+                <WhiteSpace />
+                <Logo />
+                <WingBlank>
+                <InputItem onChange={val => this.handleChange('username', val)}>username: </InputItem>
+                <WhiteSpace />
+                <InputItem type='password' onChange={val => this.handleChange('password', val)}>password: </InputItem>
+                <WhiteSpace size='md'/>
+                <Button type="primary" onClick={this.login}>Login</Button>
+                <WhiteSpace />
+                <Button onClick={this.toRegister}>Don't have an account? Please register from here</Button>
+                </WingBlank>
+            </div>
         )
     }
 }
--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,8 +1,65 @@
 import React, {Component} from 'react';
+
+import {
+    NavBar,
+    WingBlank,
+    WhiteSpace,
+    List,
+    InputItem,
+    Radio,
+    Button
+} from 'antd-mobile';
+
+import Logo from '../../components/logo/logo'; 
+
 export default class Register extends Component {
+    state = {
+        username: '',
+        password: '',
+        confirmed: '',
+        type: 'Boss'
+    };
+
+    register = () => {
+        console.log(this.state);
+    }
+
+    handleChange = (attributeName, val) => {
+        this.setState({
+            [attributeName]: val
+        });
+    }
+
+    toLogin = () => {
+        this.props.history.replace('/login');
+    }
+    
     render() {
+        const {type} = this.state;
         return (
-            <div>Register</div>
+            <div>
+                <NavBar>Job Radar</NavBar>
+                <WhiteSpace />
+                <Logo />
+                <WingBlank>
+                <InputItem onChange={val => this.handleChange('username', val)}>username: </InputItem>
+                <WhiteSpace />
+                <InputItem type='password' onChange={val => this.handleChange('password', val)}>password: </InputItem>
+                <WhiteSpace />
+                <InputItem type='password' onChange={val => this.handleChange('confirmed', val)}>Confirm: </InputItem>
+                <WhiteSpace />
+                <List.Item>
+                    <span>User type: </span>
+                    <Radio checked={type ==='Job Seeker'} onChange={() => this.handleChange('type', 'Job Seeker')}>Job Seeker</Radio>
+                    &nbsp;&nbsp;
+                    <Radio checked={type ==='Boss'} onChange={() => this.handleChange('type', 'Boss')}>Boss</Radio>
+                </List.Item>
+                <WhiteSpace size='md'/>
+                <Button type="primary" onClick={this.register}>Register</Button>
+                <WhiteSpace />
+                <Button onClick={this.toLogin}>Already have an account?</Button>
+                </WingBlank>
+            </div>
         )
     }
 }
